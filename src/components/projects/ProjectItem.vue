0a4944ec75d64b39ba3832d1af7cff20c300bfb0
MattScarthSaunders <m.scarsaund@gmail.com>
2024-06-23 15:44:05 +0100

    feat: reset project styling, separate list component for links

--- src/components/projects/ProjectItem.vue
+++ src/components/projects/ProjectItem.vue
@@ -1,18 +1,26 @@
 <template>
-  <li>
+  <li class="project">
     <img class="image" :src="project.imageUrl" />
     <h2 class="title">{{ project.title }}</h2>
     <p class="about">{{ project.about }}</p>
-    <ul class="links">
-      <a v-bind:key="index" v-for="(link, index) in project.links" :src="link"></a>
-    </ul>
+    <ProjectLinksList :links="project.links"></ProjectLinksList>
   </li>
 </template>
 
 <script setup lang="ts">
+import ProjectLinksList from './ProjectLinksList.vue'
 import type { Project } from './types'
 
 defineProps<{ project: Project }>()
 </script>
 
-<style scoped></style>
+<style scoped>
+.project {
+  list-style-type: none;
+}
+
+.image {
+  width: 50%;
+  height: 50%;
+}
+</style>
--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -11,7 +11,15 @@ import { ref } from 'vue'
 import ProjectItem from './ProjectItem.vue'
 import type { Project } from './types'
 
-const projects = ref<Project[]>([{ imageUrl: '', title: 'test', about: 'test', links: ['test'] }])
+const projects = ref<Project[]>([
+  {
+    imageUrl: 'https://cataas.com/cat',
+    title: 'This is a placeholder cat',
+    about:
+      'Using cataas.com, Im able to serve up random cat pictures in place of actual project content while developing the site',
+    links: ['https://cataas.com/cat']
+  }
+])
 </script>
 
 <style scoped></style>
